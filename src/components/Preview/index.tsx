95ddb99456ee58f969c8846b92c8af689bc45a54
Edgar Latorre <me@edgarlatorre.com.br>
2022-06-06 21:13:34 +0200

    Move preview to a component

---
+++ src/components/Preview/index.tsx
@@ -0,0 +1,80 @@
+import { toHTML } from "slack-markdown";
+import { ReactComponent as CopyIcon } from "../../assets/icons/copy.svg";
+
+type ChangeLog = {
+  type: string;
+  title: string;
+  description: string;
+  prLink: string;
+  taskLink: string;
+};
+
+export const Preview = (props: { changelog: ChangeLog }) => {
+  const { changelog } = props;
+  /**
+   * TODO: Change this implementation to use navigator clipboard as
+   * the execCommand is deprecated.
+   * https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+   */
+  const copyToClipboard = () => {
+    if (window) {
+      const selection = window.getSelection();
+      const element = document.getElementById("preview");
+
+      if (selection && element) {
+        selection.removeAllRanges();
+        const range = document.createRange();
+        range.selectNode(element);
+        selection.addRange(range);
+        document.execCommand("copy");
+        selection.removeAllRanges();
+      }
+    }
+  };
+
+  return (
+    <div className="flex flex-col w-full border-2 rounded px-2">
+      <h5 className="self-center text-lg underline">Preview</h5>
+      {changelog.description ? (
+        <button
+          onClick={() => copyToClipboard()}
+          className="place-self-end p-2"
+        >
+          <CopyIcon className="w-6 h-6" />
+        </button>
+      ) : null}
+      <div id="preview" className="preview">
+        {changelog.title ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<p>${changelog.type}: <strong>${changelog.title}</strong></p>`,
+            }}
+          ></div>
+        ) : null}
+        <br />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: toHTML(changelog.description),
+          }}
+        ></div>
+        <br />
+        {changelog.taskLink ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `:link: ${toHTML(
+                `<${changelog.taskLink}|Related Task>`
+              )}`,
+            }}
+          ></div>
+        ) : null}
+        {changelog.prLink ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `:github: ${toHTML(`<${changelog.prLink}|Related PR>`)}`,
+            }}
+          ></div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import { toHTML } from "slack-markdown";
 import { Input } from "../../components/form/Input";
 import { Select } from "../../components/form/Select";
 import { Text } from "../../components/form/Text";
-import { ReactComponent as CopyIcon } from "../../assets/icons/copy.svg";
 import { typeOptions } from "../../utils/options";
 import "../../styles/preview.css";
+import { Preview } from "../../components/Preview";
 
 function Home() {
   const [changelog, setChangelog] = useState({
@@ -15,27 +14,6 @@ function Home() {
     prLink: "",
     taskLink: "",
   });
-
-  /**
-   * TODO: Change this implementation to use navigator clipboard as
-   * the execCommand is deprecated.
-   * https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
-   */
-  const copyToClipboard = () => {
-    if (window) {
-      const selection = window.getSelection();
-      const element = document.getElementById("preview");
-
-      if (selection && element) {
-        selection.removeAllRanges();
-        const range = document.createRange();
-        range.selectNode(element);
-        selection.addRange(range);
-        document.execCommand("copy");
-        selection.removeAllRanges();
-      }
-    }
-  };
 
   return (
     <div className="flex flex-col items-center">
@@ -86,51 +64,7 @@ function Home() {
             }}
           />
         </form>
-        <div className="flex flex-col w-full border-2 rounded px-2">
-          <h5 className="self-center text-lg underline">Preview</h5>
-          {changelog.description ? (
-            <button
-              onClick={() => copyToClipboard()}
-              className="place-self-end p-2"
-            >
-              <CopyIcon className="w-6 h-6" />
-            </button>
-          ) : null}
-          <div id="preview" className="preview">
-            {changelog.title ? (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: `<p>${changelog.type}: <strong>${changelog.title}</strong></p>`,
-                }}
-              ></div>
-            ) : null}
-            <br />
-            <div
-              dangerouslySetInnerHTML={{
-                __html: toHTML(changelog.description),
-              }}
-            ></div>
-            <br />
-            {changelog.taskLink ? (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: `:link: ${toHTML(
-                    `<${changelog.taskLink}|Related Task>`
-                  )}`,
-                }}
-              ></div>
-            ) : null}
-            {changelog.prLink ? (
-              <div
-                dangerouslySetInnerHTML={{
-                  __html: `:github: ${toHTML(
-                    `<${changelog.prLink}|Related PR>`
-                  )}`,
-                }}
-              ></div>
-            ) : null}
-          </div>
-        </div>
+        <Preview changelog={changelog} />
       </div>
     </div>
   );
