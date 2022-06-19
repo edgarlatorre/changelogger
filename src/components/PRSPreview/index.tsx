2310a062d50608397f98220e49cd8b2627a1d81b
Edgar Latorre <me@edgarlatorre.com.br>
2022-06-19 18:29:47 +0200

    Add frontent pr input

---
+++ src/components/PRSPreview/index.tsx
@@ -0,0 +1,19 @@
+import { toHTML } from "slack-markdown";
+
+export const PRSPreview = (props: { prs: { [key: string]: string } }) => {
+  const prs = props.prs;
+  const links: any[] = [];
+
+  for (const label in prs) {
+    links.push(
+      <div
+        key={label}
+        dangerouslySetInnerHTML={{
+          __html: `:github: ${toHTML(`<${prs[label]}|${label}>`)}`,
+        }}
+      ></div>
+    );
+  }
+
+  return <>{links}</>;
+};
